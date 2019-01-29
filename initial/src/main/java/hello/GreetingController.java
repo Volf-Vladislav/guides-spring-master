4e080e4fcb913224ac247a3e996095be37876dcd
Georgiy-Vwfe <31819579+Georgiy-Vwfe@users.noreply.github.com>
2019-01-29 10:15:46 +0300

    refactor contacts and about

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -9,9 +9,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
 
+    @GetMapping("/contacts")
+    public String contacts(@RequestParam(name = "name", required = false, defaultValue = "sample text") String name,
+                           @RequestParam(name = "phone", required = false, defaultValue = "sample text") String phone,
+                           @RequestParam(name = "email", required = false, defaultValue = "sample text") String email,
+                           Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("phone", phone);
+        model.addAttribute("email", email);
+
+        return "contacts";
+    }
+
+    @GetMapping("/about")
+    public String about(@RequestParam(name = "education", required = false, defaultValue = "sample text") String education,
+                        @RequestParam(name = "job", required = false, defaultValue = "sample text") String job,
+                        @RequestParam(name = "projects", required = false, defaultValue = "sample text") String projects,
+                        Model model) {
+        model.addAttribute("education", education);
+        model.addAttribute("job", job);
+        model.addAttribute("projects", projects);
+        return "about";
+    }
 }
