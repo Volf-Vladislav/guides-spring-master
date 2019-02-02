97c4c5a0694fa5e7bde92375ed74d9bba93c0dff
Volf-Vladislav <43464334+Volf-Vladislav@users.noreply.github.com>
2019-02-03 02:36:49 +0300

    Update GreetingController.java

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -13,27 +13,17 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
-
-    @GetMapping("/contacts")
-    public String contacts(@RequestParam(name = "name", required = false, defaultValue = "sample text") String name,
-                           @RequestParam(name = "phone", required = false, defaultValue = "sample text") String phone,
-                           @RequestParam(name = "email", required = false, defaultValue = "sample text") String email,
-                           Model model) {
-        model.addAttribute("name", name);
-        model.addAttribute("phone", phone);
-        model.addAttribute("email", email);
-
-        return "contacts";
-    }
-
     @GetMapping("/about")
-    public String about(@RequestParam(name = "education", required = false, defaultValue = "sample text") String education,
-                        @RequestParam(name = "job", required = false, defaultValue = "sample text") String job,
-                        @RequestParam(name = "projects", required = false, defaultValue = "sample text") String projects,
-                        Model model) {
-        model.addAttribute("education", education);
-        model.addAttribute("job", job);
-        model.addAttribute("projects", projects);
+        public String about(@RequestParam(name = "about", required = false, defaultValue = "some info ab me ;D") String about, Model model) {
+        model.addAttribute("about", about);
         return "about";
+    }
+    @GetMapping("/contacts")
+    public String contacts(@RequestParam(name = "mail"  , required = false, defaultValue = "arsenkadaev@mail.ru")String mail,
+                           @RequestParam(name = "number"  , required = false, defaultValue = "+7(903)004-89-06")String number,
+                           Model model){
+        model.addAttribute("mail", mail);
+        model.addAttribute("number", number);
+        return "contacts";
     }
 }
